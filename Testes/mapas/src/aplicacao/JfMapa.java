6ecc9e030a5c12fca23b8682a2a46c87672fc250
Alex <aloucurae@yahoo.com>
2015-10-19 20:30:08 -0200

    teste mapa

--- Testes/mapas/src/aplicacao/JfMapa.java
+++ Testes/mapas/src/aplicacao/JfMapa.java
@@ -3,6 +3,8 @@ package aplicacao;
 import java.awt.Component;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 
 /**
@@ -21,6 +23,7 @@ public final class JfMapa extends javax.swing.JFrame {
 
     public JfMapa() {
         initComponents();
+       
         insereGrade(10, 10); //tamx, tamy
         //  insereAtor("Nome", 0, 0); //posx, poxy do jogador
         // insereElemento("Nomeelemetno", 0, 0);
@@ -31,6 +34,7 @@ public final class JfMapa extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,6 +45,17 @@ public final class JfMapa extends javax.swing.JFrame {
             }
         });
 
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 174, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 379, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -48,12 +63,16 @@ public final class JfMapa extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(42, 42, 42)
                 .addComponent(jButton1)
-                .addContainerGap(296, Short.MAX_VALUE))
+                .addContainerGap(567, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(373, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addGap(24, 24, 24))
         );
@@ -137,6 +156,7 @@ public final class JfMapa extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
     public JLabel criaGrade(String nome, int x, int y) {
         JLabel grade = new JLabel();
@@ -157,6 +177,8 @@ public final class JfMapa extends javax.swing.JFrame {
 
         return grade;
     }
+
+ 
 
     public void moveAtor(JLabel ator, int posX, int posY) {
         ator.setLocation(posX, posY);
