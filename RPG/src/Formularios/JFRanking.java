f562e5ee8cfe3dec53171b46dfba1bec0300930e
Fábio Klug <Fabio@Fabio-Klug.local>
2015-11-10 21:55:28 -0200

    Adicionado código
    Incrementado formulário de Ranking

--- RPG/src/Formularios/JFRanking.java
+++ RPG/src/Formularios/JFRanking.java
@@ -79,6 +79,11 @@ public class JFRanking extends javax.swing.JFrame {
         jScrollPane2.setViewportView(jTable1);
 
         jButton1.setText("SAIR");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jTextField1.setText("jTextField1");
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
@@ -144,6 +149,10 @@ public class JFRanking extends javax.swing.JFrame {
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        JFRanking.this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
